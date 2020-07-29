5965ebbfc050dbe186ce519ad0fe9f3d49622945
andypalmera <andreapalmerita@hotmail.com>
2020-07-29 19:27:42 -0300

    Updates

---
+++ precache-manifest.d6ce3cdad0d21d165b98e9be187534fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8d92ea1036b6527bf46785e2c6665e",
+    "url": "/wiki-image-app/index.html"
+  },
+  {
+    "revision": "2a54e9cac51a4d993ea3",
+    "url": "/wiki-image-app/static/css/main.a5d6b3cf.chunk.css"
+  },
+  {
+    "revision": "1a5dfcd3ff79b4123676",
+    "url": "/wiki-image-app/static/js/2.41f0d7fc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/wiki-image-app/static/js/2.41f0d7fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a54e9cac51a4d993ea3",
+    "url": "/wiki-image-app/static/js/main.3c49a7fe.chunk.js"
+  },
+  {
+    "revision": "b9abb91011e7b6ff9706",
+    "url": "/wiki-image-app/static/js/runtime-main.a05e8ac7.js"
+  }
+]);
